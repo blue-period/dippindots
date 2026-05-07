a272202a9a8184c42bc38daf40a247681015d631
Devin <Devin.Ramsden@jhuapl.edu>
2026-05-07 00:04:48 -0400

    install cleanup

---
+++ mac_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+INSTALL_DIR="$SCRIPT_DIR/installs/macOS"
+
+source "$INSTALL_DIR/bat"
+source "$INSTALL_DIR/fd"
+source "$INSTALL_DIR/fzf"
+source "$INSTALL_DIR/fzftab"
+source "$INSTALL_DIR/ripgrep"
+source "$INSTALL_DIR/zoxide"
+source "$INSTALL_DIR/zsh-vi-mode"
