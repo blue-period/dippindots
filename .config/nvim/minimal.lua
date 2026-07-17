db0b61fbaceb9dae58873ebe1ad0b65b508e4c17
Devin <Devin.Ramsden@jhuapl.edu>
2026-07-17 17:27:35 -0400

    take in cursor position from cmd line editor call

--- .config/nvim/minimal.lua
+++ .config/nvim/minimal.lua
@@ -1,5 +1,17 @@
 local source = debug.getinfo(1, "S").source
 local config_dir = vim.fn.fnamemodify(source:sub(1, 1) == "@" and source:sub(2) or source, ":p:h")
+
+local pos_file = vim.env.CMDLINE_POS_FILE
+
+if pos_file and pos_file ~= "" then
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    callback = function()
+      local col = vim.fn.col(".") - 1  -- bash READLINE_POINT is zero-based
+      vim.fn.writefile({ tostring(col) }, pos_file)
+    end,
+  })
+end
+
 vim.opt.runtimepath:prepend(config_dir)
 
 vim.opt.termguicolors = true
@@ -15,3 +27,5 @@ vim.opt.relativenumber = true
 vim.cmd("syntax enable")
 vim.cmd("filetype plugin indent on")
 vim.cmd("colorscheme gotham")
+
+
